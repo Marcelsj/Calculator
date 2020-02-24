eb993b9dcd8d02b89dabe3200e6051846ed75e4d
Marcelsj <marcel.serra@gmail.com>
2020-02-24 12:05:40 +0100

    added apply urbag

--- Calculator.py
+++ Calculator.py
@@ -1,9 +1,14 @@
 
 class Calculator:
 
-   urbag_constant = 12.4
+   
     
-    def sum(a,b):
-        return a+b
+   def sum(a,b):
+       return a+b
+   
+   def applyUrbag(a,b):
+       urbag_constant = 12.4
+       result = a*urbag_constant+b
+       return result
     
 
--- test.py
+++ test.py
@@ -5,5 +5,8 @@ result = Calculator.sum(3,4)
 print("El resultat de sumar 3 i 4 és: ",result)
 
 
+result2 = Calculator.applyUrbag(3,4)
+print("El resultat d'aplicar urbag a 3 i 4 és: ", result2)
+
 
 
